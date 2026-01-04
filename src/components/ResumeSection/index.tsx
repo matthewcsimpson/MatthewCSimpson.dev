66396bd6c0794c0fa2c06e8e8ca3126e7917307e
Matthew Simpson <mcsimpson+gh@gmail.com>
2026-01-03 20:19:10 -0800

    resume + buttons

--- src/components/ResumeSection/index.tsx
+++ src/components/ResumeSection/index.tsx
@@ -8,6 +8,28 @@ const ResumeSection = () => {
   return (
     <div className="ResumeSection">
       <SectionHeader title="resume" />
+      <div className="ResumeSection__cta">
+        <a
+          className="ResumeSection__button ResumeSection__button--resume"
+          href="/resume/MatthewSimpsonResume.pdf"
+          download
+        >
+          &darr; Download Resume
+        </a>
+        <a
+          className="ResumeSection__button ResumeSection__button--linkedin"
+          href="https://www.linkedin.com/in/matthewcsimpson/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img
+            className="ResumeSection__buttonicon"
+            src="/icons/external-link.svg"
+            alt="External link icon"
+          />{" "}
+          View LinkedIn
+        </a>
+      </div>
       <ResumeExperience experience={Resume.workExperience} />
       <ResumeEducation education={Resume.education} />
     </div>
