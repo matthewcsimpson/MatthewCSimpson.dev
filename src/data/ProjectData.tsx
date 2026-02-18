9e8e1ca53b5d14e6a919e4ce286d4adb49018fdc
Matthew Simpson <mcsimpson+gh@gmail.com>
2026-02-18 11:48:05 -0800

    justwatch

--- src/data/ProjectData.tsx
+++ src/data/ProjectData.tsx
@@ -5,6 +5,7 @@ import {
   code4renaProject,
   coordConversionProject,
   deAIGoogleProject,
+  justWatchWidgetsProject,
   rulesOfAcquisitionProject,
   whaleReportProject,
   whaleReportAdministrationProject,
@@ -12,6 +13,30 @@ import {
 } from "@/data/projects";
 
 export const projectsData: ProjectData[] = [
+  {
+    id: "justwatch-widgets",
+    title: "JustWatch Widgets",
+    group: "",
+    imageSrc: "/images/justwatch-widgets.png",
+    description:
+      "A WordPress Extension that facilitates customizing and embedding widgets from JustWatch to a website.",
+    longDescription: justWatchWidgetsProject,
+    links: [
+      {
+        type: LINK_TYPES.REPOSITORY,
+        text: "View on GitHub",
+        link: "https://github.com/matthewcsimpson/JustWatch-Widgets",
+      },
+    ],
+    tags: [
+      TAGS.WORDPRESS,
+      TAGS.JAVASCRIPT,
+      TAGS.HTML,
+      TAGS.CSS3,
+      TAGS.GIT,
+      TAGS.GITHUB,
+    ],
+  },
   {
     id: "de-ai-google",
     title: "De-AI Google",
---
+++ src/data/projects/justwatch-widgets.tsx
@@ -0,0 +1,4 @@
+export const justWatchWidgetsProject = [
+  `As the publisher of a Film & TV website using WordPress as its back end, I have always been frustrated with the clunky manner in which JustWatch is integrated with the system.  For those who don't know, JustWatch is a service which fins streaming offers for movies and tv series, allowing writers to add quick links to the titles they are talking about.`,
+  `Previously, a block of code had to be edited and then pasted into an HTML block in the post or page editor, which is unintuitive, so I developed this plugin to not only make it easier but also add a number of customizations.`,
+];
